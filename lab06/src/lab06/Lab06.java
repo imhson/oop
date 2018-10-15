d68b3f41de5182fc640c4b0c087d038631a75db6
imhson <phamhoangson99@gmail.com>
2018-10-15 15:48:06 +0700

    update lab06

--- lab06/src/lab06/Lab06.java
+++ lab06/src/lab06/Lab06.java
@@ -22,16 +22,27 @@ public class Lab06 {
         layer.add(new Rectangle(10, 2));
         layer.add(new Triagle(2, 1, 30, 40, 50));
         layer.add(new Circle(2.4));
+        System.out.println("Before");
         for (int i=0;i<layer.size();i++){
             System.out.println(layer.elementAt(i).toString());
         }
+        System.out.println("After");
         layer.deleteTriagle();
         for (int i=0;i<layer.size();i++){
             System.out.println(layer.elementAt(i).toString());
         }
+        System.out.println("Before");
         diagram.add(layer);
+        for (int i=0;i<layer.size();i++){
+            System.out.println(diagram.elementAt(0).elementAt(i).toString());
+        }
+
         diagram.deleteCircle();
-        System.out.println(diagram.elementAt(0).elementAt(0).toString());
+        System.out.println("Aftet");
+        for (int i=0;i<layer.size();i++){
+            System.out.println(diagram.elementAt(0).elementAt(i).toString());
+        }
+
 
     }
     
